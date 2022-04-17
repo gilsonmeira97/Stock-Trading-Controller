af7db9380610d5743bab2c79a517ce2b517e1c2e
Gilson Meira <gilsonmeira97@gmail.com>
2022-04-17 23:37:03 +0200

    Implementação integração com Metatrader 5

---
+++ mt5Connection.py
@@ -0,0 +1,8 @@
+from dotenv import dotenv_values
+import MetaTrader5 as mt5
+
+config_mt5 = dotenv_values(".env")
+
+if not mt5.initialize(login=int(config_mt5["LOGIN"]), password=config_mt5["PASSWORD"], server=config_mt5["SERVER"]):
+    print("Falha ao inicializar!")
+    mt5.shutdown()
