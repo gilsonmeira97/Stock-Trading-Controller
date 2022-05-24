3cbdb648db9ed133c9a8fd3e0f726fea485a28e4
Gilson Meira <gilsonzi@yahoo.com>
2022-05-25 00:51:27 +0200

    Novas estratégias

--- strategies/caseVendaCandleAlta(0.85%).py
+++ strategies/caseVendaCandleAlta(0.85%).py
@@ -7,8 +7,8 @@ from dbOperations import getConnection
 client, db = getConnection()
 nameFile = f"Extracted Venda_Candle_Alta - {str(datetime.now().timestamp()).replace('.','')}"
 symbols = getSymbols()
-f_StopGain = 0.01
-f_StopLoss = -0.0125
+f_StopGain = 0.0085
+f_StopLoss = -0.0085
 f_MinVolume = 100000
 f_MinOcurrences = 15
 f_varReference = 0.005
@@ -18,7 +18,7 @@ f_date_end = LastDate(2022,6,30)
 
 with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Date Loss', 'Date Gain', 'Volume Min', 'Volume Med', 'Var. Max'])
+    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Date Loss', 'Date Gain', 'Volume Min', 'Volume Med'])
     
     for i, symbol in enumerate(symbols):
         data_result = getDayRate(db, symbol, f_date_start, f_date_end, f_MinVolume)
@@ -34,41 +34,24 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
         acertos = 0
         percentual_acertos = 0
         maximum_loss = 0
-        max_ocilation = 0
         date_loss = None
         maximum_gain = 0
         date_gain = None
 
-        ### Inicio 1ª verificação
         for data in datas:
             open = data['open']
             close = data['close']
             high = data['high']
-            dayOfWeek = data['date'].isoweekday()
-
-            if (day_reference['close'] > day_reference['open']) and ((open / day_reference['close'] - 1) >= f_varReference) and (dayOfWeek != 5):
-                if close < open:
-                    max_var = (high / open - 1) * -1
-                    max_ocilation = max_var if max_var < max_ocilation else max_ocilation
-
-            day_reference = data
-
-        ### Inicio 2ª verificação
-        day_reference = {'close': 0, 'open': 0, 'day_volume': 0}
-
-        for data in datas:
-            open = data['open']
-            close = data['close']
-            high = data['high']
+            low = data['low']
             dayOfWeek = data['date'].isoweekday()
 
             if (day_reference['close'] > day_reference['open']) and ((open / day_reference['close'] - 1) >= f_varReference) and (dayOfWeek != 5):
                 ocurrences += 1
-
-                if ((high / open - 1) * -1) >= max_ocilation:
-                    variation = (close / open - 1) * -1
+                
+                if (low / open - 1) * -1 >= f_StopGain:
+                    variation = f_StopGain
                 else:
-                    variation = max_ocilation
+                    variation = (close / open - 1) * -1
                 
                 total_gain += variation
 
@@ -83,7 +66,6 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
                         maximum_gain = variation
                         date_gain = data['date']
 
-                last_object = None
 
             day_reference = data
 
@@ -92,7 +74,7 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
             percentual_acertos = acertos / ocurrences
         
         if (ocurrences >= f_MinOcurrences) and ((percentual_acertos > 0.65 and avg_gain > 0.003) or (percentual_acertos <= 0.25 and avg_gain < -0.003)):
-            writer.writerow([symbol, qty_datas, ocurrences, acertos, erros, percentual_acertos, total_gain, avg_gain, maximum_loss, maximum_gain, date_loss, date_gain, data_result['min_volume'], data_result['avg_volume'], max_ocilation])
+            writer.writerow([symbol, qty_datas, ocurrences, acertos, erros, percentual_acertos, total_gain, avg_gain, maximum_loss, maximum_gain, date_loss, date_gain, data_result['min_volume'], data_result['avg_volume']])
         print('Concluído: {:.2f}%'.format((i+1) / len(symbols) * 100))
 
 client.close()
