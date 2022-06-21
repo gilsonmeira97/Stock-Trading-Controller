afac23967a6cacfbb0bc3e8109a06ac2fd8118f8
Gilson Meira <gilsonmeira97@gmail.com>
2022-06-21 19:26:50 +0200

    Alteração formato CSV e alteração de valores

--- strategies/caseSegundaTerca(Sexta).py
+++ strategies/caseSegundaTerca(Sexta).py
@@ -12,12 +12,12 @@ f_StopLoss = -0.016
 f_MinVolume = 100000
 f_MinOcurrences = 15
 f_varReference = -0.025
-f_date_start = FirstDate(2021,5,1)
-f_date_end = LastDate(2022,5,30)
+f_date_start = FirstDate(2021,12,15)
+f_date_end = LastDate(2022,6,30)
 
 with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Volume Min', 'Volume Med'])
+    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Date Loss', 'Date Gain', 'Volume Min', 'Volume Med'])
     
     for i, symbol in enumerate(symbols):
         data_result = getDayRate(db, symbol, f_date_start, f_date_end, f_MinVolume, [1, 2, 3, 4, 5])
@@ -35,16 +35,19 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
         percentual_acertos = 0
         maximum_loss = 0
         maximum_gain = 0
+        date_loss = None
+        date_gain = None
 
         for data in datas:
             open = data['open']
             close = data['close']
             dayOfWeek = data['date'].isoweekday()
-            if (dayOfWeek in [3, 4, 5]):
+
+            if (dayOfWeek in [1, 4, 5]):
                 last_friday = data
-            elif (dayOfWeek == 1):
+            elif (dayOfWeek == 2):
                 last_object = data
-            elif (last_object != None) and (last_friday != None) and (dayOfWeek == 2) and (last_friday['close'] / last_friday['open'] - 1) > f_varReference and (last_friday['date'] < last_object['date']):
+            elif (last_object != None) and (last_friday != None) and (dayOfWeek == 3) and (last_friday['close'] / last_friday['open'] - 1) > f_varReference and (last_friday['date'] < last_object['date']):
                 ocurrences += 1
                 variation = (open / last_object['close'] - 1)
 
@@ -52,10 +55,14 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
 
                 if (variation <= 0):
                     erros += 1
-                    maximum_loss = variation if variation < maximum_loss else maximum_loss
+                    if variation < maximum_loss:
+                        maximum_loss = variation 
+                        date_loss = data['date']
                 else:
                     acertos += 1
-                    maximum_gain = variation if variation > maximum_gain else maximum_gain
+                    if variation > maximum_gain:
+                        maximum_gain = variation
+                        date_gain = data['date']
 
                 last_object = None
 
@@ -64,7 +71,7 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
             percentual_acertos = acertos / ocurrences
         
         if (ocurrences >= f_MinOcurrences) and ((percentual_acertos > 0.65 and avg_gain > 0.003) or (percentual_acertos <= 0.25 and avg_gain < -0.003)):
-            writer.writerow([symbol, qty_datas, ocurrences, acertos, erros, percentual_acertos, total_gain, avg_gain, maximum_loss, maximum_gain, data_result['min_volume'], data_result['avg_volume']])
+            writer.writerow([symbol, qty_datas, ocurrences, acertos, erros, percentual_acertos, total_gain, avg_gain, maximum_loss, maximum_gain, date_loss, date_gain, data_result['min_volume'], data_result['avg_volume']])
         print('Concluído: {:.2f}%'.format((i+1) / len(symbols) * 100))
 
 client.close()
