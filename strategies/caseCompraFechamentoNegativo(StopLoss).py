d11a47b71606162d4fc1fb23e946db0982013798
Gilson Meira <gilsonmeira97@gmail.com>
2022-07-11 20:28:30 +0200

    Nova estratégia

--- strategies/caseCompraFechamentoNegativo(StopLoss).py
+++ strategies/caseCompraFechamentoNegativo(StopLoss).py
@@ -5,19 +5,19 @@ import csv
 from dbOperations import getConnection
 
 client, db = getConnection()
-nameFile = f"Extracted Venda_Candle_Alta - {str(datetime.now().timestamp()).replace('.','')}"
+nameFile = f"Extracted Fechamento Negativo - {str(datetime.now().timestamp()).replace('.','')}"
 symbols = getSymbols()
-f_StopGain = 0.006
-f_StopLoss = -0.013
+f_StopGain = 0.008
+f_StopLoss = -0.016
 f_MinVolume = 100000
 f_MinOcurrences = 15
-f_varReference = 0.005
+f_varReference = -0.025
 f_date_start = FirstDate(2021,12,15)
 f_date_end = LastDate(2022,6,30)
 
 with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Date Loss', 'Date Gain', 'Volume Min', 'Volume Med', 'Stop Loss'])
+    writer.writerow(['Ativo', 'Qtd Registros', 'Ocorrencias', 'Acertos', 'Erros', '% Acerto', 'G/L Total', "G/L Med.", 'Max. Loss', 'Max. Gain', 'Date Loss', 'Date Gain', 'Volume Min', 'Volume Med', 'Stop_loss'])
     
     for i, symbol in enumerate(symbols):
         data_result = getDayMinutes(db, symbol, f_date_start, f_date_end, f_MinVolume)
@@ -25,7 +25,8 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
         data_result = data_result[0]
         datas = data_result['ticks']
         qty_datas = len(datas)
-        day_reference = {'close': 0, 'open': 0, 'day_volume': 0}
+        last_object = None
+        last_friday = None
         ocurrences = 0
         total_gain = 0
         avg_gain = 0
@@ -33,28 +34,12 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
         acertos = 0
         percentual_acertos = 0
         maximum_loss = 0
-        max_ocilation = 0
-        date_loss = None
         maximum_gain = 0
+        date_loss = None
         date_gain = None
+        max_ocilation = 0
 
         ### Inicio 1ª verificação
-        for data in datas:
-            open = data['open']
-            close = data['close']
-            high = data['high']
-            dayOfWeek = data['date'].isoweekday()
-
-            if (day_reference['close'] > day_reference['open']) and ((open / day_reference['close'] - 1) >= f_varReference):
-                if close < open:
-                    max_var = (high / open - 1) * -1
-                    max_ocilation = max_var if max_var < max_ocilation else max_ocilation
-
-            day_reference = data
-
-        ### Inicio 2ª verificação
-        day_reference = {'close': 0, 'open': 0, 'day_volume': 0}
-
         for data in datas:
             open = data['open']
             close = data['close']
@@ -62,26 +47,61 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
             ticks = data['ticks']
             dayOfWeek = data['date'].isoweekday()
 
-            if (day_reference['close'] > day_reference['open']) and ((open / day_reference['close'] - 1) >= f_varReference):
+            if (last_object != None):
+                openClose = ((open / last_object['close']) - 1)
+
+                if openClose >= f_StopGain:
+                    var_test = openClose
+                elif ((high / last_object['close']) - 1) >= f_StopGain:
+                    var_test = f_StopGain
+                else:
+                    var_test = (close / last_object['close'] - 1)
+                if var_test > 0:
+                    for tick in ticks:
+                        variation_tick = (tick['low'] / last_object['close'] - 1)
+                        max_ocilation =  variation_tick if variation_tick < max_ocilation else max_ocilation
+                        
+                        if (tick['high'] / last_object['close'] - 1) >= f_StopGain:
+                            break
+                        
+                last_object = None
+
+            if dayOfWeek != 5:
+                last_object = data
+
+        ### Inicio 2ª verificação
+
+        last_object = None
+        max_ocilation += 0.0005    
+
+        for data in datas:
+            open = data['open']
+            close = data['close']
+            high = data['high']
+            ticks = data['ticks']
+            dayOfWeek = data['date'].isoweekday()
+            variation = None
+
+            if (last_object != None):
                 ocurrences += 1
-                variation = None
-                isStopMovel = False
+                openClose = ((open / last_object['close']) - 1)
+
                 for tick in ticks:
-                    if ((tick['high'] / open - 1) * -1) < max_ocilation and not isStopMovel:
+                    if openClose >= f_StopGain:
+                        variation = openClose
+                        break
+                    elif openClose < max_ocilation:
+                        variation = openClose
+                        break
+                    elif ((tick['low'] / last_object['close']) - 1) <= max_ocilation:
                         variation = max_ocilation
                         break
-                    elif ((tick['low'] / open - 1) * -1) >= f_StopGain and not isStopMovel:
+                    elif ((tick['high'] / last_object['close']) - 1) >= f_StopGain:
                         variation = f_StopGain
-                        isStopMovel = True
-                        continue
-                    elif (isStopMovel) and ((tick['high'] / open - 1) * -1) > variation:
-                        variation = ((tick['close'] / open - 1) * -1)
-                        continue
-                    elif (isStopMovel) and ((tick['high'] / open - 1) * -1) <= variation:
                         break
 
-                variation = ((close / open - 1) * -1) if (variation is None) else variation
-                
+                variation = close / last_object['close'] - 1 if variation is None else variation
+
                 total_gain += variation
 
                 if (variation <= 0):
@@ -95,7 +115,10 @@ with open(f'extracteds/{nameFile}.csv', mode='w', newline='') as file:
                         maximum_gain = variation
                         date_gain = data['date']
 
-            day_reference = data
+                last_object = None
+
+            if dayOfWeek != 5:
+                last_object = data
 
         if (ocurrences > 0):
             avg_gain = total_gain / ocurrences
